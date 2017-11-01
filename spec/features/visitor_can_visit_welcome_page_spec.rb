50d836dc0f66df2e3fba29f48bb11c38db1f377d
amhursh <amhursh@gmail.com>
2017-11-01 01:26:09 -0600

    Fix typo in test

--- spec/features/visitor_can_visit_welcome_page_spec.rb
+++ spec/features/visitor_can_visit_welcome_page_spec.rb
@@ -4,7 +4,7 @@ feature 'Visitor can visit welcome page' do
   scenario 'and can see site title and login link' do
     visit '/'
 
-    expect(page).to have_content 'CitizenSentinal'
+    expect(page).to have_content 'CitizenSentinel'
     expect(page).to have_content 'Sign Up'
     expect(page).to have_content 'Sign In'
   end
