0d64249a6ab8ddee8cff43d8b526add713615a84
Aaron Hursh <amhursh@gmail.com>
2017-11-05 14:03:35 -0700

    Add spec for user adding issues

---
+++ spec/features/issues/user_can_add_issues_to_track_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+feature 'User can add issues' do
+  scenario 'user can select from list of issues and add them to profile' do
+    user = create(:user)
+    issue1, issue2, issue3 = create_list(:issue, 3)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit user_path(user)
+
+    expect(page).to_not have_content(issue1.name)
+    expect(page).to_not have_content(issue2.name)
+    expect(page).to_not have_content(issue3.name)
+
+    expect(page).to have_content 'Add Issues You Would Like to Track'
+
+    click_on 'Add Issues You Would Like to Track'
+
+    expect(current_path).to eq new_user_issues_path
+
+    check "check-issue-#{issue1.id}"
+    check "check-issue-#{issue3.id}"
+
+    click 'Add Issues'
+
+    expect(current_path).to eq user_path(user)
+
+    expect(page).to have_content issue1.name
+    expect(page).to have_content issue3.name
+    expect(page).to_not have_content issue2.name
+
+    expect(page).to have_content "Recent Activity for #{issue1.name}"
+    expect(page).to have_content "Recent Activity for #{issue3.name}"
+  end
+end
