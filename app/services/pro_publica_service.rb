1313aa9375b4689759a380ec7ac0b14279f3c083
Aaron Hursh <amhursh@gmail.com>
2017-11-06 15:56:29 -0700

    Add propublica service and bills by issue method

---
+++ app/services/pro_publica_service.rb
@@ -0,0 +1,19 @@
+class ProPublicaService
+
+  def initialize
+    @conn = Faraday.new("https://api.propublica.org") do |f|
+      f.headers["X-API-Key"] = ENV["PROPUBLICA_API_KEY"]
+      f.adapter Faraday.default_adapter
+    end
+  end
+
+  def bills_by_issue(issue)
+    response = @conn.get("/congress/v1/bills/subjects/#{issue}.json")
+    binding.pry
+    JSON.parse(response.body, symbolize_names: true)[:results]
+  end
+
+  def self.bills_by_issue(issue)
+    new.bills_by_issue(issue)
+  end
+end
