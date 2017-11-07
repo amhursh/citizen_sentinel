94e7fbf9b12d7e058f227bc93622e73e6cdcaaea
Aaron Hursh <amhursh@gmail.com>
2017-11-06 22:23:32 -0700

    Fix typo

--- app/controllers/bill_follows_controller.rb
+++ app/controllers/bill_follows_controller.rb
@@ -3,7 +3,7 @@ class BillFollowsController < ApplicationController
   def create
     user = current_user
     bill_id = format_bill_id(params["bill_id"])
-    bill = Billsearch.find_and_generate(params[:bill_id])
+    bill = BillsSearch.find_and_generate(params[:bill_id])
     user.bills << bill
   end
 
