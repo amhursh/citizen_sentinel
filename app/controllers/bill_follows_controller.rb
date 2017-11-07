8eb70fb4b11dfd0c27210e25e3ca41b48f604529
Aaron Hursh <amhursh@gmail.com>
2017-11-06 23:47:44 -0700

    Complete logic for create action

--- app/controllers/bill_follows_controller.rb
+++ app/controllers/bill_follows_controller.rb
@@ -4,7 +4,14 @@ class BillFollowsController < ApplicationController
     user = current_user
     bill_id = format_bill_id(params["bill_id"])
     bill = BillsSearch.find_and_generate(bill_id)
-    user.bills << bill
+    if user.bills.include?(bill)
+      flash[:success] = "Already following Bill: #{bill.bill_id}"
+      redirect_back(fallback_location: user_path(user))
+    else
+      user.bills << bill
+      flash[:success] = %Q[Followed Bill: #{bill.bill_id}. <a href="/users/#{user.id}">See This Bill on Your Profile</a>]
+      redirect_back(fallback_location: user_path(user))
+    end
   end
 
   private
