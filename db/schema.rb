efbccc6c6f8994ea4ddd35351bf1b92e988ab112
Aaron Hursh <amhursh@gmail.com>
2017-11-06 23:34:56 -0700

    Add enacted attribute to bills

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171107054059) do
+ActiveRecord::Schema.define(version: 20171107061632) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20171107054059) do
     t.boolean "active"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "enacted"
   end
 
   create_table "issues", force: :cascade do |t|
