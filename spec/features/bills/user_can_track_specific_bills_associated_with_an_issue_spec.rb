3c325a24f2af8d41d5ab34427d7665cba0bbc233
Aaron Hursh <amhursh@gmail.com>
2017-11-06 21:16:37 -0700

    Add spec for user adding bills

---
+++ spec/features/bills/user_can_track_specific_bills_associated_with_an_issue_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+feature 'User can track specific bills' do
+  scenario 'user can view issue activity and add associated bill to track' do
+    user = create(:user)
+    issue = create(:issue, name: 'Health', slug: 'health')
+    user.issues << issue
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit user_path(user)
+
+    click_on 'Followed Issues'
+    click_on 'Check Activity'
+
+    expect(current_path).to eq users_issue_path(issue)
+
+    click_on 'Follow this Bill', match: :first
+
+    expect(page).to eq users_issue_path(issue)
+    expect(page).to have_content 'Added Bill:'
+    expect(page).to have_css '.followed-bill'
+
+    bill = Bill.last
+
+    click_on 'Profile'
+
+    click_on 'Followed Bills'
+
+    expect(page).to have_content bill.name
+    expect(page).to have_content bill.short_title
+    expect(page).to have_content bill.subject
+  end
+end
