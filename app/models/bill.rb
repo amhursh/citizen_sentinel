cfa9abda0164aabae723de3b9e720a31128a7667
Aaron Hursh <amhursh@gmail.com>
2017-11-06 23:35:48 -0700

    Add working from_pro_publica method

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -1,17 +1,19 @@
 class Bill < ApplicationRecord
 
-  def from_pro_publica(raw_bill)
+  def self.from_pro_publica(raw_bill)
     bill = find_by(bill_id: raw_bill[:bill_slug]) || Bill.new
     bill.attributes = {
-      bill_id: info[:uid],
-      username: info[:info][:name],
-      email: info[:info][:email],
-      password: info[:uid],
-      oauth_token: info[:credentials][:token],
-      oauth_expire: info[:credentials][:expires_at],
-      image: info[:info][:image]
+      bill_id: raw_bill[:bill_slug],
+      title: raw_bill[:short_title],
+      subject: raw_bill[:primary_subject],
+      active: raw_bill[:active],
+      enacted: raw_bill[:enacted]
     }
+    binding.pry
     bill.save
     bill
   end
+
+
+
 end
