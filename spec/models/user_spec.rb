19a79e336b63fefe9c3d9ce0ba45b2b61e70cc62
Aaron Hursh <amhursh@gmail.com>
2017-11-08 12:23:05 -0700

    Cleanup

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,37 @@
-require 'rails_helper'
+# require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+# RSpec.describe User, type: :model do
+#   context 'class methods' do
+#     it '.from_oauth' do
+#       raw_data = {
+#         :provider=>"facebook",
+#         :uid=>"12345",
+#         :info=> {
+#           :email=>"aaron@aaron.com",
+#           :name=>"aaron",
+#           :image=>"http://via.placeholder.com/150x150"
+#         },
+#         credentials:=> {
+#           :token=> "abcdefg123456",
+#           :expires_at=>1514962006,
+#           :expires=>true
+#         },
+#         :extra=> {
+#           :raw_info=> {
+#             :name=>"aaron",
+#             :email=>"aaron@aaron.com",
+#             :id=>"12345"
+#           }
+#         }
+#       }
+
+#       User.from_oauth(raw_data)
+
+#       user = User.last
+
+#       expect(user.username).to eq 'aaron'
+#       expect(user.email).to eq 'aaron@aaron.com'
+#       expect(user.oauth_token).to eq "abcdefg123456"
+#     end
+#   end
+# end
